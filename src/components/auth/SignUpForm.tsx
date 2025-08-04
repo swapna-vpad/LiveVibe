4849c0b98664f44a453938df1427cca46782b326
swapna-vpad <swapus93@gmail.com>
2025-08-04 18:20:18 +0530

    Updated SignUpForm.tsx

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -358,9 +358,7 @@ const onClose = () => {
             <div className="absolute inset-0 flex items-center">
               <span className="w-full border-t border-gray-300" />
             </div>
-            <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">Or continue with</span>
-            </div>
+           
           </div>
           
           {/* Spotify Login Button */}
